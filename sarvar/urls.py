d3528cf71740d0b981dd6c934df19b3d6dd4ccdd
Sarvar Abdullajonov <152815541+sarvar0911@users.noreply.github.com>
2024-02-01 04:59:53 +0500

    -started project -added Post model -adjusted html files -adjusted static files

---
+++ sarvar/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from sarvar.views import HomeView, PostDetailView
+
+appname = "blog"
+urlpatterns = [
+    path('', HomeView.as_view(), name="homepage"),
+
+    path('post/<int:pk>', PostDetailView.as_view(), name="post_detail"),
+]
--- sarvar/views.py
+++ sarvar/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, DetailView, DeleteView, UpdateView, ListView
 
-# Create your views here.
+from sarvar.models import Post
+
+
+class HomeView(View):
+    def get(self, request):
+        posts_for_homepage = Post.objects.order_by('-created_at')[:10]
+        context = {
+            "posts": posts_for_homepage,
+        }
+
+        return render(request, 'home.html', context=context)
+
+
+class PostDetailView(DetailView):
+    template_name = "post_detail.html"
+    model = Post
+    context_object_name = "post"
